ec2ef61a101efb9ff3e12f5ddfbac423906ae86a
Christophe Martineau <ti-tophe@hotmail.com>
2015-09-19 16:29:39 -0400

    BD updatée

--- V1/V1_Site/Models/Models.Designer.cs
+++ V1/V1_Site/Models/Models.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La génération de code T4 est activée pour le modèle 'C:\Users\Christophe\Desktop\École\École\Troisième Année\Première Session\Projet de fin d'études\V1\V1_Site\Models\Models.edmx'. 
+﻿// La génération de code T4 est activée pour le modèle 'C:\Users\Christophe\Desktop\École\École\Troisième Année\Première Session\Projet de fin d'études\Artiste\V1\V1_Site\Models\Models.edmx'. 
 // Pour activer la génération de code héritée, définissez la valeur de la propriété
 // du concepteur 'Stratégie de génération de code' sur 'ObjectContext hérité'. Cette propriété est disponible dans la fenêtre Propriétés lorsque le modèle
 //  est ouvert dans le concepteur.
